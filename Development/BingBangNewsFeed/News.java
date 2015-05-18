52dc8896048f84a8bd5437444f5634f06f900c61
Paul <h2opaulh2o@users.noreply.github.com>
2015-05-18 14:10:44 +0300

    Create News.java
    The News class will be used as a container for the data extracted from multiple RSS

---
+++ Development/BingBangNewsFeed/News.java
@@ -0,0 +1,28 @@
+
+public class News {
+	private String title, description, link;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+}
