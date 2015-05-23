b2dca7890f2ae867e76a1c821f2a40aa91506a0e
Cosmin <cosmin.cazacu@asii.ro>
2015-05-23 03:11:58 +0300

    Versiunea 2.0 (Functional)

---
+++ Versiunea 2.0/src/newsfeed/XmlReader.java
@@ -0,0 +1,107 @@
+
+package newsfeed;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.Characters;
+import javax.xml.stream.events.XMLEvent;
+
+
+public class XmlReader {
+
+	 static final String TITLE = "title";
+	  static final String DESCRIPTION = "description";
+	  static final String CHANNEL = "channel";
+	  static final String LINK = "link";
+	  static final String ITEM = "item";
+	  static final String PUB_DATE = "pubDate";
+	  static final String GUID = "guid";
+
+	  final URL url;
+
+	  public XmlReader(String feedUrl) {
+	    try {
+	      this.url = new URL(feedUrl);
+	    } catch (MalformedURLException e) {
+	      throw new RuntimeException(e);
+	    }
+	  }
+
+	  public GetRss readFeed() {
+	    GetRss feed = null;
+	    try {
+	      boolean isFeedHeader = true;
+	      String description = "";
+	      String title = "";
+	      String link = "";
+	      String pubdate = "";
+	      XMLInputFactory inputFactory = XMLInputFactory.newInstance();
+	      InputStream in = read();
+	      XMLEventReader eventReader = inputFactory.createXMLEventReader(in);
+	      while (eventReader.hasNext()) {
+	        XMLEvent event = eventReader.nextEvent();
+	        if (event.isStartElement()) {
+	          String localPart = event.asStartElement().getName()
+	              .getLocalPart();
+	          switch (localPart) {
+	          case ITEM:
+	            if (isFeedHeader) {
+	              isFeedHeader = false;
+	              feed = new GetRss(title, link, description,  pubdate);
+	            }
+	            event = eventReader.nextEvent();
+	            break;
+	          case TITLE:
+	            title = getCharacterData(event, eventReader);
+	            break;
+	          case DESCRIPTION:
+	            description = getCharacterData(event, eventReader);
+	            break;
+	          case LINK:
+	            link = getCharacterData(event, eventReader);
+	            break;
+	          case PUB_DATE:
+	            pubdate = getCharacterData(event, eventReader);
+	            break;
+	          }
+	        } else if (event.isEndElement()) {
+	          if (event.asEndElement().getName().getLocalPart() == (ITEM)) {
+	            SetRss message = new SetRss();        
+	            message.setDescription(description);
+	            message.setLink(link);
+	            message.setTitle(title);
+	            feed.getMessages().add(message);
+	            event = eventReader.nextEvent();
+	            continue;
+	          }
+	        }
+	      }
+	    } catch (XMLStreamException e) {
+	      throw new RuntimeException(e);
+	    }
+	    return feed;
+	  }
+
+	  private String getCharacterData(XMLEvent event, XMLEventReader eventReader)
+	      throws XMLStreamException {
+	    String result = "";
+	    event = eventReader.nextEvent();
+	    if (event instanceof Characters) {
+	      result = event.asCharacters().getData();
+	    }
+	    return result;
+	  }
+
+	  private InputStream read() {
+	    try {
+	      return url.openStream();
+	    } catch (IOException e) {
+	      throw new RuntimeException(e);
+	    }
+	  }
+	} 
