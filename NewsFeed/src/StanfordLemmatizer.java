9ed8f1b9f33725fbf3b47aee85a6bf9247db9b20
ionution <ionut.gheorghitanu@gmail.com>
2015-05-26 12:21:39 +0300

    Create StanfordLemmatizer.java

---
+++ NewsFeed/src/StanfordLemmatizer.java
@@ -0,0 +1,52 @@
+//Mai este de lucrat la aceasta metoda. Face parte din NewsFeed pentru clasa Newsfilter.
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Properties;
+
+import edu.stanford.nlp.ling.CoreAnnotations.LemmaAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.util.CoreMap;
+
+public class StanfordLemmatizer {
+
+    protected StanfordCoreNLP pipeline;
+
+    public StanfordLemmatizer() {
+        Properties props;
+        props = new Properties();
+        props.put("annotators", "tokenize, ssplit, pos, lemma");
+
+
+        this.pipeline = new StanfordCoreNLP(props);
+    }
+
+    public List<String> lemmatize(String documentText)
+    {
+        List<String> lemmas = new LinkedList<String>();        
+        Annotation document = new Annotation(documentText);        
+        this.pipeline.annotate(document);     
+        List<CoreMap> sentences = document.get(SentencesAnnotation.class);
+        for(CoreMap sentence: sentences) {
+            for (CoreLabel token: sentence.get(TokensAnnotation.class)) {
+                lemmas.add(token.get(LemmaAnnotation.class));
+            }
+        }
+        return lemmas;
+    }
+
+    
+
+    //Test
+    public static void main(String[] args) {
+        System.out.println("Starting Stanford Lemmatizer");
+        String text = "Learning\n";
+        StanfordLemmatizer slem = new StanfordLemmatizer();
+        System.out.println(slem.lemmatize(text));
+    }
+
+}
